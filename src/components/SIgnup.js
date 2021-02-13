c93b3963ed73c2d74ae61329d340abab47c6723a
DananjayaBC <Dana.chanuka@gmail.com>
2021-02-14 00:21:02 +0530

    Create routes

--- src/components/SIgnup.js
+++ src/components/SIgnup.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
+import {Link, useHistory} from "react-router-dom"
 
 
 export default function Signup() {
@@ -8,13 +9,29 @@ export default function Signup() {
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
   const { signup } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
 
-  function handleSubmit(e) {
+
+ async function handleSubmit(e) {
     e.preventDefault()
 
-    
+    if(passwordRef.current.value !==
+      passwordConfirmRef.current.value){
+        return setError("Passwords do not match")
+      }
 
-    signup(emailRef.current.value, passwordRef.current.value)
+      try{
+        setError("")
+        setLoading(true)
+        await signup(emailRef.current.value, passwordRef.current.value)
+        history.push("/")
+      }catch{
+        setError("Failed to create an account")
+      }
+
+    setLoading(false)
   }
   
  
@@ -23,8 +40,9 @@ export default function Signup() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-        
-          <Form >
+          
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -37,14 +55,14 @@ export default function Signup() {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button className="w-100" type="submit">
+            <Button disabled={loading} className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account?
+        Already have an account? <Link to="/login">Log In</Link>
       </div>
     </>
   )
