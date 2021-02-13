7e8f43ac4d66c991378b916b4e4382e3be62b546
DananjayaBC <Dana.chanuka@gmail.com>
2021-02-13 22:09:32 +0530

    Connect to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from 'firebase/app'
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MASSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID 
+    apiKey: "AIzaSyCWM9L8OqeHOgtvwMEyE0QjpIoswM6kjTg",
+    authDomain: "onlinetrainticket-17091.firebaseapp.com",
+    databaseURL: "https://onlinetrainticket-17091-default-rtdb.firebaseio.com",
+    projectId: "onlinetrainticket-17091",
+    storageBucket: "onlinetrainticket-17091.appspot.com",
+    messagingSenderId: "427044396791",
+    appId: "1:427044396791:web:21402a60331851a5407df2",
+    measurementId: "G-HFMCYLBB77"
 })
 
 export const auth = app.auth()
